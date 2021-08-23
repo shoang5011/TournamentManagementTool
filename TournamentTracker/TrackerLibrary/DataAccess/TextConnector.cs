760ca992713a0156f9b594dc49ef0de23bdbf5a5
shoang5011 <s.hoang5011@gmail.com>
2021-08-23 09:10:23 -0500

    add save data to text file method

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers; 
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv"; 
         //TODO - Wire up the CreatePrize for text files
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-            return model;
+            // Load text file and convert to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            //Find the max ID
+
+            int currentId = 1; 
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+            
+            //Add new record 
+            prizes.Add(model);
+
+            //Convert prizes to list<string> and save to file
+            prizes.SaveToPrizeFile(PrizesFile);
+
+            return model; 
         }
     }
 }
