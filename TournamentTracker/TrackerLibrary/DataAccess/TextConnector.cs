b56d04f5f07c660a0099874a6ffbb6129624ea99
shoang5011 <s.hoang5011@gmail.com>
2021-09-08 17:58:41 -0500

    finished team form bindings for both text and sql

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -11,7 +11,8 @@ namespace TrackerLibrary.DataAccess
     public class TextConnector : IDataConnection
     {
         private const string PrizesFile = "PrizeModels.csv";
-        private const string PeopleFile = "PersonModels.csv"; 
+        private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv"; 
 
         public PersonModel CreatePerson(PersonModel model)
         {
@@ -57,6 +58,21 @@ namespace TrackerLibrary.DataAccess
             //Convert prizes to list<string> and save to file
             prizes.SaveToPrizeFile(PrizesFile);
 
+            return model; 
+        }
+
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            int currentId = 1;
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+            teams.Add(model);
+            teams.SaveToTeamFile(TeamFile);
             return model; 
         }
 
